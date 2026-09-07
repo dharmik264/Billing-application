81242caaf603aa91b23d6ef499b1d0c4ef1845d8
Balkanji Bari <info@balkanjibari.org>
2026-09-07 12:12:33 +0530

    Update build.sh to use migrate_schemas for django-tenants

--- backend/build.sh
+++ backend/build.sh
@@ -4,6 +4,6 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py migrate
+python manage.py migrate_schemas
 python seed_plans.py
 python seed_initial_data.py
